25d5e617d997e3ed8a6c447fb425f1fe6237bdf9
hj-vero <hj.veronica.shim@gmail.com>
2025-12-05 22:53:05 +0900

    chore: Prettier 설치 및 config, ignore 설정

---
+++ src/shared/configs/prettier.config.js
@@ -0,0 +1,20 @@
+/**
+ * @see https://prettier.io/docs/configuration
+ */
+const config = {
+  printWidth: 120,
+  tabWidth: 2,
+  useTabs: false,
+  semi: true,
+  singleQuote: true,
+  jsxSingleQuote: false, // JSX 속성 값에 single quote 사용
+  quoteProps: "as-needed", // 객체 속성에 필요할 때만 quotation
+  trailingComma: "all",
+  bracketSpacing: true,
+  objectWrap: "collapse", // 객체 한 줄로 유지
+  bracketSameLine: false,
+  arrowParens: "always", // parameter 1개인 arrow 함수의 괄호 유지
+  endOfLine: "auto",
+};
+
+export default config;
